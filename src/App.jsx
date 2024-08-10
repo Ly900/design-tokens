4ead6a4220e41635a2f3b4209b555f844b7c2f5d
Ly Nguyen <lynguyendc@gmail.com>
2024-08-10 10:41:17 -0400

    Swap container and header

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,21 @@ function App() {
 
 	return (
 		<>
-			<div className="header">
-				<div className="component-wrapper">
-					<div className="component-wrapper__inner-wrapper">
+			<div className="component-wrapper component-wrapper__header">
+				<div className="component-wrapper__inner-wrapper">
+					<div className="header">
 						<img src="/design-tokens/assets/globe.svg" className="globe-icon" />
 						<h1 className="mb-0 text-5xl">Theming With Design Tokens</h1>
+					</div>
+				</div>
+			</div>
+
+			<div className="themes">
+				<div className="component-wrapper">
+					<div className="component-wrapper__inner-wrapper">
+						<div className="buttons-wrapper">
+							<button>Dark mode</button>
+						</div>
 					</div>
 				</div>
 			</div>
